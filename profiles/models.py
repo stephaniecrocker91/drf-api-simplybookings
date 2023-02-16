1fdb3cf4e00c1430ef2ff52d592d41c6324a35e8
Stephie <stephaniecrocker91@gmail.com>
2023-02-16 10:26:14 +0000

    Create Profile model, add to admin.py and run migrations, install Pillow

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    # otp_code = models.CharField(max_length=6, unique=True, null=True)
+    # email_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    image = models.ImageField(
+        upload_to="images/", default="../default_profile_rexhwh"
+    )
+    content = models.TextField(blank=True)
     
+    class Meta:
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
