454025af4e6dba125617df309e48a600fdb17385
Stephie <stephaniecrocker91@gmail.com>
2023-02-16 15:32:40 +0000

    Added profile serializer and list views

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
 
     'profiles',
 ]
---
+++ profiles/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id',
+            'owner',
+            'username',
+            'email',
+            'phone_number',
+            'created_at',
+            'updated_at',
+            'image',
+            'content',
+            ]
