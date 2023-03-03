f39d5c7b654d2dc8f541234b3a85ae1ac643c73a
Stephie <stephaniecrocker91@gmail.com>
2023-03-03 11:53:13 +0000

    Create Servicedetail view fetching by service id

--- services/views.py
+++ services/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Service
@@ -7,4 +8,17 @@ class ServiceList(APIView):
     def get(self, request):
         services = Service.objects.all()
         serializer = ServiceSerializer(services, many=True)
+        return Response(serializer.data)
+
+class ServiceDetail(APIView):
+    def get_object(self, pk):
+        try:
+            service = Service.objects.get(pk=pk)
+            return service
+        except Service.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        service = self.get_object(pk)
+        serializer = ServiceSerializer(service)
         return Response(serializer.data)
